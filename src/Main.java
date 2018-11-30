7d5bea15d7440a2bbb45cb351c1a8e6aa5af4ad5
MahmouedMohamed <mahmouedmartin222@yahoo.com>
2018-11-30 07:58:37 +0200

    Palindrome 20160231

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,22 @@
-
-public class Main {
-	public static void main(String [] args)
+import java.util.*;
+public class test {
+	public boolean Palindrome(int [] Array)
 	{
-		
+	    for(int i=0;i<Array.length/2;i++)
+	        {if( Array[i] != Array[Array.length-1-i] ) //added Palindrome Function
+	            {return false; } }
+	    return true;
 	}
+	public static boolean Palindrome(String s)
+	{
+	    for(int i=0;i<s.length()/2;i++)
+	        {if( s.charAt(i) != s.charAt(s.length()-1-i) )
+	            {return false;} }
+	    return true;
+	}
+	public static void main(String[] args) {
+		String shit;
+		shit=new Scanner(System.in).nextLine();
+	}
+
 }
